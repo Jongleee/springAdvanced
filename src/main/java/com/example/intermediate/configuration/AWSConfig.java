7fa21a716f2dea2dfb5aecbba9341e5330b279cd
DESKTOP-MVDMJ4A\Jonglee <forin777@gmail.com>
2022-08-06 19:41:14 +0900

    개인정보보호

--- src/main/java/com/example/intermediate/configuration/AWSConfig.java
+++ src/main/java/com/example/intermediate/configuration/AWSConfig.java
@@ -4,17 +4,18 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AWSConfig {
-
-    /**
-     * Key는 중요정보이기 때문에 properties 파일에 저장한 뒤 가져와 사용하는 방법이 좋습니다.
-     */
-    private String iamAccessKey = "AKIARV26BY754VUB3PGK"; // IAM Access Key
-    private String iamSecretKey = "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"; // IAM Secret Key
+    @Value("${iamAccessKey}")
+    private String iamAccessKey;
+    @Value("${iamSecretKey}")
+    private String iamSecretKey;
+    //properties 파일에   iamAccessKey=액세스 키
+    //iamSecretKey=시크릿 키 추가
     private String region = "ap-northeast-2"; // Bucket Region
 
     @Bean
